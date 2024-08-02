4210acf18fff9889afdab681fcb29edf2a519419
Oz Elentok <oz.elen@gmail.com>
2024-08-03 01:28:05 +0300

    feat(update_manager): Allow origin and requirements files override of klipper and moonraker entries

--- moonraker/components/update_manager/common.py
+++ moonraker/components/update_manager/common.py
@@ -42,7 +42,8 @@ BASE_CONFIG: Dict[str, Dict[str, str]] = {
     }
 }
 
-OPTION_OVERRIDES = ("channel", "pinned_commit", "refresh_interval")
+OPTION_OVERRIDES = ("channel", "pinned_commit", "refresh_interval", "origin",
+                    "requirements", "system_requirements", "install_script")
 
 class AppType(ExtendedEnum):
     NONE = 1
